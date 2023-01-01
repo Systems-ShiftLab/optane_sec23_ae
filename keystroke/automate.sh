731ea8d854991b042c5bf240280f6954e2fb8c24
Suraaj Kanniwadi <suraaj@shiftlab12.cs.virginia.edu>
2022-12-31 19:39:48 -0500

    Incorporate changes

--- keystroke/automate.sh
+++ keystroke/automate.sh
@@ -5,6 +5,7 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$script_dir"
 
 target_backing_file=${target_backing_file:-/mnt/pmem0/ws-server-keystroke}
+prober_file=${prober_file:-/mnt/pmem0/rmw-prober-keystroke}
 ws_port=${ws_port:-4321}
 
 export remote_ip=${remote_ip:-10.0.0.1}
@@ -16,8 +17,8 @@ remote_cmd () {
 server_cmd=${prober_cmd:-optane_sec23_se/keystroke/server/start_bg.sh}
 server_kill=${prober_cmd:-optane_sec23_se/keystroke/server/end_bg.sh}
 
-prober_cmd=${prober_cmd:-optane_sec23_se/keystroke/prober/rmw_prober.sh}
-prober_kill=${prober_kill:-killall rmw_prober}
+prober_cmd=${prober_cmd:-optane_sec23_se/keystroke/prober/start_bg.sh}
+prober_kill=${prober_kill:-optane_sec23_se/keystroke/prober/kill_bg.sh}
 prober_dir=$(dirname $prober_cmd)
 prober_trace=$prober_dir/rmw_trace
 
@@ -31,8 +32,10 @@ trap exit_function SIGINT
 
 [[ -d keystrokes/keystroke100 ]] || ./download-dataset.sh
 
-remote_cmd "$prober_kill; $prober_cmd $target_backing_file 2>errlog > $prober_trace &"
+remote_cmd $prober_kill
 remote_cmd "$server_kill; $server_cmd $target_backing_file $ws_port"
+exit
+remote_cmd "$prober_cmd $target_backing_file $prober_trace" #TODO startbg, endbg?
 
 sleep 10
 echo "Starting client"
