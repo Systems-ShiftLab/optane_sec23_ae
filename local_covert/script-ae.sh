a83ed51ab60de636463a9399b8c584f0d2d1f7e8
Suraaj Kanniwadi <suraaj@shiftlab12.cs.virginia.edu>
2023-01-07 21:56:07 -0500

    Add remote run script

---
+++ local_covert/script-ae.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "$script_dir"
+
+export remote_ip=10.0.0.1
+export remote_user=suraajks
+
+
+../common/remote_commands/generic.sh optane_sec23_ae/local_covert/local-run.sh
+
+mkdir -p result-ae
+scp $remote_user@$remote_ip:optane_sec23_ae/local_covert/results/covert_results.dat result-ae
