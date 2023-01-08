68adb333d1dc9679ae23e79ac1d35f1811fed842
Suraaj Kanniwadi <suraaj@shiftlab12.cs.virginia.edu>
2023-01-07 21:25:42 -0500

    Gen stats for local covert channel

---
+++ local_covert/gen_final.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+import sys
+import re
+
+def main():
+    if len(sys.argv) != 5:
+        print(f'Usage: {sys.argv[0]} <ait_stats> <rmw_stats> <cont_stats> <output_name>' )
+        return
+
+    aitfile = sys.argv[1]
+    rmwfile = sys.argv[2]
+    contfile = sys.argv[3]
+    outfile = sys.argv[4]
+
+    def gen_stats(ifile):
+        from statistics import mean,stdev
+        with open(ifile) as fd:
+            acclst = []
+            bwlst = []
+            for line in fd:
+                try:
+                    acc,bw = [float(x) for x in line.strip().split(',')]
+                except ValueError:
+                    continue
+                acc = acc * 100 #Percent
+                bw = bw / 1000 #kbps
+                acclst.append(acc)
+                bwlst.append(bw)
+            acc_mean = mean(acclst)
+            acc_stdev = stdev(acclst)
+            bw_mean = mean(bwlst)
+            bw_stdev = stdev(bwlst)
+            return acc_mean,acc_stdev,bw_mean,bw_stdev
+
+
+    ofd = open(outfile,'w')
+
+    def helper(nametag,filename):
+        tmp = gen_stats(filename)
+        ofd.write(f'{nametag}_acc_mean = {tmp[0]:0.2f}\n')
+        ofd.write(f'{nametag}_acc_stdev = {tmp[1]:0.2f}\n')
+        ofd.write(f'{nametag}_kbps_mean = {tmp[2]:0.2f}\n')
+        ofd.write(f'{nametag}_kpbs_stdev = {tmp[3]:0.2f}\n')
+
+    helper("ait",aitfile)
+    helper("rmw",rmwfile)
+    helper("cont",contfile)
+        
+    ofd.close()
+
+
+if __name__ == '__main__':
+    main()
--- local_covert/local-run.sh
+++ local_covert/local-run.sh
@@ -4,30 +4,40 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$script_dir"
 
 rep_count=${rep_count:-110}
+msg_size=${msg_size:-1000}
 exps=${exps:-'cont ait rmw'}
-res_dir=${res_dir:-result-ae}
+res_dir=${res_dir:-results}
 
+rm -rf $res_dir
 mkdir -p $res_dir
 
 make receiver_epoch sender_epoch
 
+tsc_mhz=$(dmesg | grep "tsc: Detected" | grep -o  '[0-9]*.[0-9]* MHz' | awk '{print $1}')
+tsc_mhz=${tsc_mhz:-2100}
+
+tsc=$(echo "$tsc_mhz * (10 ^ 6)" | bc -l)
+
 for exp in $exps ; do
-    echo accuracy,cycles >  $res_dir/$exp
     for ((i=0;i<rep_count;i++)) ; do
         echo "Running exp: $exp, Iteration: $i"
         echo "Running receiver"
-        ./receiver_epoch $exp 1000 > recvr_temp_log &
+        ./receiver_epoch $exp $msg_size > recvr_temp_log &
         sleep 3 #Measured init time
         echo "Running sender"
-        ./sender_epoch $exp 1000 > sender_temp_log
+        ./sender_epoch $exp $msg_size > sender_temp_log
         wait
         rcvr_epoch=$(grep "Target epoch" recvr_temp_log)
         snder_epoch=$(grep "Target epoch" sender_temp_log)
         [[ $rcvr_epoch == $snder_epoch ]] || { echo Sync mistake ;  continue ; }
-        accuracy=$(tail -n1 recvr_temp_log)
-        time=$(tail -n1 sender_temp_log)
-        echo $accuracy,$time | tee -a $res_dir/$exp
+        correct=$(tail -n1 recvr_temp_log)
+        cycles=$(tail -n1 sender_temp_log)
+
+        bw=$(echo "($tsc * $msg_size) / ($cycles) " | bc -l )
+        accuracy=$(echo "($correct) / $msg_size" | bc -l )
+        echo $accuracy,$bw | tee -a $res_dir/$exp
     done
 done
 
 rm sender_temp_log recvr_temp_log
+./gen_final.py $res_dir/ait $res_dir/rmw $res_dir/cont $res_dir/covert_results.dat
