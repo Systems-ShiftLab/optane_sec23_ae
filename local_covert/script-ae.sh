31bfc39612ac7a1ecc6f1746e6e1922d48117c7f
Suraaj Kanniwadi <suraaj@shiftlab12.cs.virginia.edu>
2023-01-10 11:38:23 -0500

    Keeping things uniform

--- local_covert/script-ae.sh
+++ local_covert/script-ae.sh
@@ -3,9 +3,8 @@
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$script_dir"
 
-export remote_ip=10.0.0.1
-export remote_user=suraajks
-
+export remote_ip=${remote_ip:-10.0.0.1}
+export remote_user=${remote_user:-suraajks}
 
 ../common/remote_commands/generic.sh optane_sec23_ae/local_covert/local-run.sh
 
--- remote_covert/script-ae.sh
+++ remote_covert/script-ae.sh
@@ -3,9 +3,10 @@ set -e
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$script_dir"
 
-kv_ip=${kv_ip:-10.0.0.1}
-kv_port=${kv_port:-1111}
-kv_file=${kv_file:-/mnt/pmem0/wearlevel-covert}
+export remote_ip=${remote_ip:-10.0.0.1}
+export kv_port=${kv_port:-1111}
+export kv_backing_file=${kv_backing_file:-/mnt/pmem0/wearlevel-covert}
+
 rep_count=${rep_count:-3}
 score_threshold=${score_threshold:-1}
 
@@ -19,10 +20,10 @@ end_wl() {
 }
 
 start_remote_server() {
-    kv_port=$kv_port remote_ip=$kv_ip kv_backing_file=$kv_file ../common/remote_commands/start_kv_server.sh
+    ../common/remote_commands/start_kv_server.sh
 }
 stop_remote_server() {
-    remote_ip=$kv_ip ../common/remote_commands/kill_kv_server.sh
+    ../common/remote_commands/kill_kv_server.sh
 }
 
 exit_fn() {
@@ -51,10 +52,10 @@ single_run() {
         echo Sleep end
         echo "Iteration: $i"
         echo "Running receiver"
-        taskset -c 10 ./receiver $kv_ip $kv_port > recvr_temp_log_$i &
+        taskset -c 10 ./receiver $remote_ip $kv_port > recvr_temp_log_$i &
         echo "Running sender"
         time_start=$(date +%s.%N)
-        taskset -c 16-23 ./sender $kv_ip $kv_port > sender_temp_log_$i
+        taskset -c 16-23 ./sender $remote_ip $kv_port > sender_temp_log_$i
         time_end=$(date +%s.%N)
         time=$(echo "$time_end - $time_start" | bc)
         wait
