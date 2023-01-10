13b961dd388f646276dd05aeda55ba5e08d0f404
Suraaj Kanniwadi <suraaj@shiftlab12.cs.virginia.edu>
2023-01-10 01:03:19 -0500

    Add final report

---
+++ runall-ae.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -e
+
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "$script_dir"
+
+progress_file=${progress_file:-.overall-progress}
+
+truncate $progress_file -s0
+for i in $(find -name "script-ae.sh") ; do
+    echo Running $i | tee -a $progress_file
+    (
+        cd $(dirname $i)
+        ./$(basename $i)
+    )
+done
+
+cp $(find -name '*.csv' | grep result-ae ) report/results_csv
+cp $(find -name '*.dat' | grep result-ae ) report/results_dat
+
+echo Generating Report
+cd report
+make
